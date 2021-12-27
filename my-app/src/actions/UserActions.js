f4c0827a09162fe3d28b24e7e31e4e670e5e09e6
marinouj <marinoujoanna@gmail.com>
2021-12-27 16:16:01 +0200

    api_fix6

--- my-app/src/actions/UserActions.js
+++ my-app/src/actions/UserActions.js
@@ -1,10 +1,8 @@
-const host = "34.64.219.179";
-
 /**Responsible for contacting the server
  * and returning all the users
  */
 export const loadUsers = async () => {
-  const newData = await fetch("http://" + host + ":8080/users", {
+  const newData = await fetch("http://34.64.219.179:8080/users", {
     method: "GET",
     headers: {
       "Content-Type": "application/json",
@@ -20,7 +18,7 @@ export const loadUsers = async () => {
  */
 export const verifyUser = async (user) => {
   console.log(user);
-  const newData = await fetch("http://" + host + ":8080/login", {
+  const newData = await fetch("http://34.64.219.179:8080/login", {
     method: "POST",
     body: JSON.stringify({ name: user.username, password: user.password }),
     headers: {
@@ -35,7 +33,7 @@ export const verifyUser = async (user) => {
 /**Responsible to get users notifications/info
  */
 export const getUser = async (id) => {
-  const newData = await fetch("http://" + host + ":8080/getUser", {
+  const newData = await fetch("http://34.64.219.179:8080/getUser", {
     method: "POST",
     body: JSON.stringify({ id: id }),
     headers: {
@@ -50,7 +48,7 @@ export const getUser = async (id) => {
  * and adding a user
  */
 export const createUser = async (user) => {
-  await fetch("http://" + host + ":8080/createUser", {
+  await fetch("http://34.64.219.179:8080/createUser", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -65,7 +63,7 @@ export const createUser = async (user) => {
  * and confirming a user registration
  */
 export const confirmUser = async (id) => {
-  const newData = await fetch("http://" + host + ":8080/confirmUser", {
+  const newData = await fetch("http://34.64.219.179:8080/confirmUser", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -80,7 +78,7 @@ export const confirmUser = async (id) => {
  * and updating a user
  */
 export const updateUser = async (user) => {
-  const newData = await fetch("http://" + host + ":8080/updateUser", {
+  const newData = await fetch("http://34.64.219.179:8080/updateUser", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -95,7 +93,7 @@ export const updateUser = async (user) => {
  * and deleting a user
  */
 export const deleteUser = async (id) => {
-  const newData = await fetch("http://" + host + ":8080/deleteUser", {
+  const newData = await fetch("http://34.64.219.179:8080/deleteUser", {
     method: "POST",
     body: JSON.stringify({ id: id }),
     headers: {
