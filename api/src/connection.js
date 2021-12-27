070756aa95cf4ee3dae6cf8b1cc1af679b4ed717
marinouj <marinoujoanna@gmail.com>
2021-12-27 18:53:56 +0200

    api_fix9

--- api/src/connection.js
+++ api/src/connection.js
@@ -2,10 +2,14 @@ const mongoose = require("mongoose");
 
 const User = require("./User.model");
 
-const connection = "mongodb://127.0.0.1:27017/local";
+var argv = require("optimist").argv;
+
+// configuration =================
+
+const connection = "mongodb://127.0.0.1:27017/";
 
 const connectDb = () => {
-  return mongoose.connect(connection);
+  return mongoose.connect("mongodb://" + argv.be_ip + ":80/my_database");
 };
 
 module.exports = connectDb;
